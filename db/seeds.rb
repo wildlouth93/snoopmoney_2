4250f3e0da700c85f5d08bb8dc4f98488e0cf44c
Willie Louth <willielouth@gmail.com>
2019-11-22 10:24:09 -0600

    worked on seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,22 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Holding.destroy_all
+WatchListItem.destroy_all
+User.destroy_all
 
-user1 = User.create( email: 'smuser1@gmail.com', first_name: 'Sm1', last_name: 'User1', account_balance: 2500, password: 'hunter2');
+user1 = User.create( email: 'u28@gmail.com', first_name: 'Demo', last_name: 'User', account_balance: 45000, password: 'hunter2');
 user2 = User.create( email: 'smuser2@gmail.com', first_name: 'Sm2', last_name: 'User2', account_balance: 2500, password: 'hunter2');
 user3 = User.create( email: 'smuser3@gmail.com', first_name: 'Sm3', last_name: 'User3', account_balance: 2500, password: 'hunter2');
 user4 = User.create( email: 'smuser4@gmail.com', first_name: 'Sm4', last_name: 'User4', account_balance: 2500, password: 'hunter2');
 user5 = User.create( email: 'smuser5@gmail.com', first_name: 'Sm5', last_name: 'User5', account_balance: 2500, password: 'hunter2');
 
 holding1 = Holding.create( user_id: user1.id, ticker: 'MSFT', num_shares: 10, cost_basis: 50);
-holding2 = Holding.create( user_id: user2.id, ticker: 'MSFT', num_shares: 10, cost_basis: 50);
-holding3 = Holding.create( user_id: user3.id, ticker: 'MSFT', num_shares: 10, cost_basis: 50);
-holding4 = Holding.create( user_id: user4.id, ticker: 'MSFT', num_shares: 10, cost_basis: 50);
-holding5 = Holding.create( user_id: user5.id, ticker: 'MSFT', num_shares: 10, cost_basis: 50);
-
+holding2 = Holding.create( user_id: user1.id, ticker: 'AMZN', num_shares: 10, cost_basis: 50);
+holding3 = Holding.create( user_id: user1.id, ticker: 'APPL', num_shares: 10, cost_basis: 50);
+holding4 = Holding.create( user_id: user1.id, ticker: 'GE', num_shares: 10, cost_basis: 50);
+holding5 = Holding.create( user_id: user1.id, ticker: 'FB', num_shares: 10, cost_basis: 50);
 
 watch_list_item1 = WatchListItem.create( user_id: user1.id, ticker: 'FB');
-watch_list_item2 = WatchListItem.create( user_id: user2.id, ticker: 'FB');
-watch_list_item3 = WatchListItem.create( user_id: user3.id, ticker: 'FB');
-watch_list_item4 = WatchListItem.create( user_id: user4.id, ticker: 'FB');
-watch_list_item5 = WatchListItem.create( user_id: user5.id, ticker: 'FB');
+watch_list_item2 = WatchListItem.create( user_id: user1.id, ticker: 'AMZN');
+watch_list_item3 = WatchListItem.create( user_id: user1.id, ticker: 'UA');
+watch_list_item4 = WatchListItem.create( user_id: user1.id, ticker: 'GOOGL');
+watch_list_item5 = WatchListItem.create( user_id: user1.id, ticker: 'APPL');
